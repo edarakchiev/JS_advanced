9affb9d5eee9894b06fbe00c076b80c9e81e047f
edarakchiev <e_darakchiev@yahoo.com>
2021-09-24 07:37:39 +0300

    Create 01.arrayWithDelimiter.js

---
+++ 02.arrays/exercise/01.arrayWithDelimiter.js
@@ -0,0 +1,21 @@
+function array(list, sep) {
+    console.log(list.join(sep))
+}
+
+array(['One',
+        'Two',
+        'Three',
+        'Four',
+        'Five'],
+    '-'
+)
+
+
+array(['How about no?',
+        'I',
+        'will',
+        'not',
+        'do',
+        'it!'],
+    '_'
+)
