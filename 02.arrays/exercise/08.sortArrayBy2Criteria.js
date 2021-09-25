5fbbb762927128ab52dc5ec9a2ccd8e1d957b0d0
Emil Darakchiev <72097603+edarakchiev@users.noreply.github.com>
2021-09-25 12:30:54 +0300

    Create 08.sortArrayBy2Criteria.js

---
+++ 02.arrays/exercise/08.sortArrayBy2Criteria.js
@@ -0,0 +1,15 @@
+function sortArray(list) {
+    list.sort((a, b) => {
+        if (a.length - b.length === 0){
+            return a.localeCompare(b)
+        }
+        else {
+            return a.length - b.length
+        }
+    })
+    console.log(list.join("\n"))
+}
+//
+// sortArray(['alpha', 'beta', 'gamma'])
+// sortArray(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George'])
+sortArray(['test', 'Deny', 'omen', 'Default'])
