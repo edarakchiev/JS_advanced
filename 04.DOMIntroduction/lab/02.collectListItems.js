81eccc00f325d36365354b75e7db78e8f1d587b8
edarakchiev <e_darakchiev@yahoo.com>
2021-10-02 13:39:39 +0300

    add exercise

--- 04.DOMIntroduction/lab/02.collectListItems.js
+++ 04.DOMIntroduction/lab/02.collectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+    const items = document.getElementById('items').children;
+    const result = [];
+    for (let item of Array.from(items)){
+        result.push(item.textContent);
+    }
+    document.getElementById('result').textContent = result.join('\n');
 }
