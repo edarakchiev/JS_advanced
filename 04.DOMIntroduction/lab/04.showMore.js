b95d17d71ef2d720946f0a5bd3ea63418fe818bc
edarakchiev <e_darakchiev@yahoo.com>
2021-10-02 14:11:42 +0300

    add exercise

---
+++ 04.DOMIntroduction/lab/04.showMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let text = document.getElementById('text')
+    text.style.display = 'inline'
+    let btn = document.getElementById('more')
+    btn.style.display = 'none'
+}
