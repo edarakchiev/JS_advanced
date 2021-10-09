42a489fc26a1391c1de1622f9de2b206c27f1365
edarakchiev <e_darakchiev@yahoo.com>
2021-10-09 18:00:07 +0300

    Update app.js

--- 05.DOMManipulation/exercise/03. Locked-Profile/app.js
+++ 05.DOMManipulation/exercise/03. Locked-Profile/app.js
@@ -3,16 +3,18 @@ function lockedProfile() {
     parent.addEventListener('click', onClick)
 
     function onClick(ev) {
-        let isActive = ev.target.parentElement.querySelector('input[type="radio"][value="unlock"]').checked
+        let profile = ev.target.parentElement
+        let isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked
+        console.log(isActive)
         if (ev.target.tagName === 'BUTTON' && isActive) {
-
-            let dataDiv = Array.from(ev.target.parentElement.querySelectorAll('div')).find(d => d.id.includes('HiddenFields'))
-            if (ev.target.textContent === "Show more"){
-                ev.target.textContent = "Hide it"
-                dataDiv.style.display = 'block'
+            let  hiddenField = Array.from(profile.querySelectorAll('div'))
+                .find(d => d.id.includes("HiddenFields"))
+            if (ev.target.textContent === 'Show more'){
+                ev.target.textContent = 'Hide it'
+                hiddenField.style.display = 'block'
             } else {
-                ev.target.textContent = "Show more"
-                dataDiv.style.display = ''
+                ev.target.textContent = 'Show more'
+                hiddenField.style.display = ''
             }
         }
     }
