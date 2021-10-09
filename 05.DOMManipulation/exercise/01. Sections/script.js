51796f72cb5841161df5c926214cbf8366416109
edarakchiev <e_darakchiev@yahoo.com>
2021-10-09 12:52:30 +0300

    Update script.js

--- 05.DOMManipulation/exercise/01. Sections/script.js
+++ 05.DOMManipulation/exercise/01. Sections/script.js
@@ -1,3 +1,15 @@
 function create(words) {
-   console.log('TODO:...');
+   const parent = document.getElementById('content')
+   parent.addEventListener('click', onClick)
+   for (let word of words){
+      let divElement = document.createElement('div')
+      let para = document.createElement('p')
+      para.textContent = word
+      para.style.display = 'none'
+      divElement.appendChild(para)
+      parent.appendChild(divElement)
+   }
+   function onClick(ev) {
+      ev.target.querySelector('P').style.display = ''
+   }
 }
