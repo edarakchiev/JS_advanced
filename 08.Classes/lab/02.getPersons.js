f99bdb98dfac8c36ff44140c4b19371f2b90f25a
edarakchiev <e_darakchiev@yahoo.com>
2021-10-16 17:42:08 +0300

    Create 02.getPersons.js

---
+++ 08.Classes/lab/02.getPersons.js
@@ -0,0 +1,23 @@
+function getPerson() {
+    class Person {
+        constructor(firsName, lastName, age, email) {
+            this.firstName = firsName
+            this.lastName = lastName
+            this.age = age
+            this.email = email
+        }
+
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+        }
+    }
+
+    return [
+        new Person('Anna', 'Simpson', 22, 'anna@yahoo.com'),
+        new Person('SoftUni'),
+        new Person('Stephan', 'Johnson', 25),
+        new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com')
+    ]
+
+
+}
