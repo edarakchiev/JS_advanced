51a58ff4671ce88f0c8a6baa7ca46e5deb59a99b
edarakchiev <e_darakchiev@yahoo.com>
2021-10-20 19:23:19 +0300

    Update solution.js

--- examPreparation/01.Pet_Me - Resources/solution.js
+++ examPreparation/01.Pet_Me - Resources/solution.js
@@ -4,25 +4,22 @@ function solve() {
     let adoptedPet = document.querySelector('#adopted ul')
     let addBtn = document.querySelector('#container button')
     addBtn.addEventListener('click', addPet)
-
     const input = {
         name: fields[0],
         age: fields[1],
         kind: fields[2],
         owner: fields[3]
     }
-
     function addPet(e) {
         e.preventDefault()
-        let name = input.name.value
-        const age = Number(input.age.value)
-        const kind = input.kind.value
-        const owner = input.owner.value
+        let name = input.name.value.trim()
+        const age = Number(input.age.value.trim())
+        const kind = input.kind.value.trim()
+        const owner = input.owner.value.trim()
 
-        if (name === "" || input.age.value === "" || Number.isNaN(age) || kind === "" || owner === "") {
+        if (name === "" || input.age.value.trim() === "" || Number.isNaN(age) || kind === "" || owner === "") {
             return
         }
-
 
         let pet = document.createElement('li')
         let p = document.createElement('p')
@@ -36,9 +33,7 @@ function solve() {
         pet.appendChild(spanEl)
         pet.appendChild(contactBtn)
         petList.appendChild(pet)
-
         Array.from(fields).forEach(f => f.value = "")
-
         function contact() {
             contactBtn.remove()
             let divEl = document.createElement('div')
@@ -51,9 +46,12 @@ function solve() {
             divEl.appendChild(yesBtn)
             pet.appendChild(divEl)
         }
-    }
 
+    }
     function adopt(pet, inputField) {
+        if (inputField.value === ""){
+            return
+        }
         pet.querySelector('div').remove()
         let checkBtn = document.createElement('button')
         checkBtn.textContent = 'Checked'
@@ -62,7 +60,6 @@ function solve() {
         pet.querySelector('span').textContent = `New Owner: ${inputField.value}`
         adoptedPet.appendChild(pet)
     }
-
     function checked(pet) {
         pet.remove()
     }
