5bde27eeebf56fe3563446a44a1b0f6fe56f0e74
edarakchiev <e_darakchiev@yahoo.com>
2021-10-28 16:23:29 +0300

    Update 02.SummerCamp.js

--- examPreparation/02.SummerCamp.js
+++ examPreparation/02.SummerCamp.js
@@ -19,7 +19,7 @@ class SummerCamp {
         }
         let isActive = false
         for (let el of this.listOfParticipants) {
-            if (el.name !== undefined) {
+            if (el.name === name) {
                 isActive = true
             }
         }
@@ -36,9 +36,63 @@ class SummerCamp {
         return `The ${name} was successfully registered.`
     }
     unregisterParticipant (name) {
-        
+        let isActive = false
+        for (let el of this.listOfParticipants) {
+            if (el.name === name) {
+                isActive = true
+                const index = this.listOfParticipants.indexOf(el)
+                this.listOfParticipants.splice(index, 1)
+            }
+        }
+        if (isActive) {
+            return `The ${name} removed successfully.`
+        } else {
+            throw new Error (`The ${name} is not registered in the camp.`)
+        }
     }
+    timeToPlay (typeOfGame, participant1, participant2){
+        let isActive1 = false
+        let isActive2 = false
+        let condition1
+        let condition2
+        let power1
+        let power2
+        for (let el of this.listOfParticipants) {
+            if (el.name === participant1) {
+                isActive1 = true
+                condition1 = el.condition
+                power1 = el.power
+            }
+            if (el.name === participant2){
+                isActive2 = true
+                condition2 = el.condition
+                power2 = el.power
+            }
+        }
 
+        if (typeOfGame === 'WaterBalloonFights'){
+            if (!isActive1 || !isActive2) {
+                throw new Error('Invalid entered name/s.')
+            }
+            if (condition1 !== condition2){
+                throw new Error('Choose players with equal condition.')
+            }
+
+
+        } else if (typeOfGame === 'Battleship'){
+            if (!isActive1){
+                throw new Error('Invalid entered name.')
+            }
+
+            for (let el of this.listOfParticipants) {
+                if (el.name === name) {
+                    el.power1 += 20
+                }
+            }
+            return `The ${participant1} successfully completed the game ${typeOfGame}.`
+
+        }
+    }
 }
 
 
@@ -48,7 +102,16 @@ class SummerCamp {
 // console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
 // console.log(summerCamp.registerParticipant("Leila Wolfe", "childd", 200));
 
+// const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+// console.log(summerCamp.unregisterParticipant("Petar"));
+// console.log(summerCamp.unregisterParticipant("Petar Petarson"));
+
+
 const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
 console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
-console.log(summerCamp.unregisterParticipant("Petar"));
-console.log(summerCamp.unregisterParticipant("Petar Petarson"));
+console.log(summerCamp.timeToPlay("Battleship", "Petar Petarson"));
+console.log(summerCamp.registerParticipant("Sara Dickinson", "child", 200));
+// console.log(summerCamp.timeToPlay("WaterBalloonFights", "Petar Petarson", "Sara Dickinson"));
+console.log(summerCamp.registerParticipant("Dimitur Kostov", "student", 300));
+console.log(summerCamp.timeToPlay("WaterBalloonFights", "Petar Petarson", "Dimitur Kostov"));
